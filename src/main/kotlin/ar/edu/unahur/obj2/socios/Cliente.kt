8721e3ca1f9cadf0b3d53c3664ebd2f11b8d4b21
lucianoaraus <lucianoaraus97@gmail.com>
2021-05-10 10:12:50 -0300

    Update: 'Cambio de parametros EstadoDeAnimo/propina' -> Arreglado. Tests en marcha

--- src/main/kotlin/ar/edu/unahur/obj2/socios/Cliente.kt
+++ src/main/kotlin/ar/edu/unahur/obj2/socios/Cliente.kt
@@ -8,7 +8,7 @@ object Cliente { //Patron Singleton aplicado
     lateinit var barrio: Barrio
 
     //Getters
-    fun consultarPropina() = this.estadoDeAnimo.propina(pedido,bolsillo) //TRATAR DE SACAR BOLSILLO DEL PARAMETRO YA QUE SOLO SE UTILIZA EN 1 ESTADO DE ANIMO
+    fun consultarPropina() = this.estadoDeAnimo.propina(this)
     fun consultarPropinaBarrio() = this.barrio.cuenta(this.consultarPropina())
 
     //Setters
@@ -19,25 +19,25 @@ object Cliente { //Patron Singleton aplicado
 }
 
 interface EstadoDeAnimo {
-    fun propina(pedido: Pedido,bolsillo: Double) : Double
+    fun propina(cliente: Cliente) : Double
 }
 
 // Como los estados son unicos y no cambian en si, pasaron de ser clases a objetos.
 
 object Enojado : EstadoDeAnimo {
-    override fun propina(pedido: Pedido,bolsillo: Double) = 0.0 //Pedido
+    override fun propina(cliente: Cliente) = 0.0 //Pedido
 }
 
 object Feliz : EstadoDeAnimo {
-    override fun propina(pedido: Pedido,bolsillo: Double) = pedido.precio * 0.25 //Deja como propina el 25% de lo que salió el pedido
+    override fun propina(cliente: Cliente) = cliente.pedido.precio * 0.25 //Deja como propina el 25% de lo que salió el pedido
 }
 
 object Indiferente : EstadoDeAnimo {
-    override fun propina(pedido: Pedido,bolsillo: Double) = bolsillo //Deja como propina lo que tenga en el bolsillo.
+    override fun propina(cliente: Cliente) = cliente.bolsillo //Deja como propina lo que tenga en el bolsillo.
 }
 
 object Resfriado : EstadoDeAnimo {
-    override fun propina(pedido: Pedido,bolsillo: Double) = pedido.precio //Le invade el sentimiento de culpa y le deja de propina el 100% de lo que salió el pedido.
+    override fun propina(cliente: Cliente) = cliente.pedido.precio //Le invade el sentimiento de culpa y le deja de propina el 100% de lo que salió el pedido.
 }
 
 
--- src/test/kotlin/ar/edu/unahur/obj2/socios/ClienteTest.kt
+++ src/test/kotlin/ar/edu/unahur/obj2/socios/ClienteTest.kt
@@ -1,15 +1,25 @@
 package ar.edu.unahur.obj2.socios
 
 import io.kotest.core.spec.style.DescribeSpec
+import io.kotest.matchers.shouldBe
 
 class ClienteTest : DescribeSpec({
-
   // 1.Saber cuánta propina dejaría un/a cliente para un pedido de un determinado importe, teniendo en cuenta su estado de ánimo.
   // 2.Incorporar a la cuenta anterior la variación que pueda surgir según el barrio.
 
-  describe("Un/a cliente") {
-    it(''){
-    //....shouldBeTrue()
+  val pedidoPizza = Pedido(1000.0)
+  val pedidoEmpanadas = Pedido(1500.0)
+  val pedidoMilanesa = Pedido(500.0)
+  val pedidoHelado = Pedido(150.0)
+
+
+  //Hacer 2 tests por cada cliente con estado de animo diferente (enojado,feliz,indiferente,resfriado)
+  //Luego, hacer lo mismo pero aplicando los barrios (lasRosas,lasLauchas,barrioVerde,lasTorres)
+
+  describe("Un/a cliente enojado") {
+    it("Va a dejar N propina para un pedido de M importe"){
+    //.Saber cuánta propina dejaría un/a cliente para un pedido de un determinado importe, teniendo en cuenta su estado de ánimo.
+      Cliente.consultarPropina().shouldBe(100.0)//Corregir valor
     }
   }
 
