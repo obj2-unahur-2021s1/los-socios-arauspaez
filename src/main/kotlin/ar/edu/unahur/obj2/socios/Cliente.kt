a4598fd26ff37f98f6877133d1f167c72cf0731f
Julian Paez <jepaez92@gmail.com>
2021-05-09 23:50:29 -0300

    Cambio de parametros EstadoDeAnimo/propina

--- src/main/kotlin/ar/edu/unahur/obj2/socios/Cliente.kt
+++ src/main/kotlin/ar/edu/unahur/obj2/socios/Cliente.kt
@@ -3,28 +3,28 @@ package ar.edu.unahur.obj2.socios
 class Cliente(var estadoDeAnimo: EstadoDeAnimo,var bolsillo: Double,var barrio: Barrio) {
     lateinit var pedido : Pedido
     fun hacerPedido(comida: Pedido){ this.pedido = comida }
-    fun consultarPropina() = this.estadoDeAnimo.propina(pedido,bolsillo) //TRATAR DE SACAR BOLSILLO DEL PARAMETRO YA QUE SOLO SE UTILIZA EN 1 ESTADO DE ANIMO
+    fun consultarPropina() = this.estadoDeAnimo.propina(this) //TRATAR DE SACAR BOLSILLO DEL PARAMETRO YA QUE SOLO SE UTILIZA EN 1 ESTADO DE ANIMO
     fun consultarPropinaBarrio() = this.barrio.cuenta(this.consultarPropina())
     fun cambiarBarrio(nuevoBarrio: Barrio) { barrio = nuevoBarrio}
     fun cambiarEstadoDeAnimo(nuevoEstado: EstadoDeAnimo) { estadoDeAnimo = nuevoEstado }
     fun cambiarBolsillo(nuevoBolsillo: Double) { bolsillo = nuevoBolsillo }
 }
 interface EstadoDeAnimo {
-    fun propina(pedido: Pedido,bolsillo: Double) : Double
+    fun propina(cliente: Cliente) : Double
 }
 
 class Enojado() : EstadoDeAnimo {
-    override fun propina(pedido: Pedido,bolsillo: Double) = 0.0 //Pedido
+    override fun propina(cliente: Cliente) = 0.0 //Pedido
 }
 
 class Feliz() : EstadoDeAnimo {
-    override fun propina(pedido: Pedido,bolsillo: Double) = pedido.precio * 0.25 //Deja como propina el 25% de lo que salió el pedido
+    override fun propina(cliente: Cliente) = cliente.pedido.precio * 0.25 //Deja como propina el 25% de lo que salió el pedido
 }
 
 class Indiferente() : EstadoDeAnimo {
-    override fun propina(pedido: Pedido,bolsillo: Double) = bolsillo //Deja como propina lo que tenga en el bolsillo.
+    override fun propina(cliente: Cliente) = cliente.bolsillo //Deja como propina lo que tenga en el bolsillo.
 }
 
 class Resfriado() : EstadoDeAnimo {
-    override fun propina(pedido: Pedido,bolsillo: Double) = pedido.precio//Le invade el sentimiento de culpa y le deja de propina el 100% de lo que salió el pedido.
+    override fun propina(cliente: Cliente) = cliente.pedido.precio//Le invade el sentimiento de culpa y le deja de propina el 100% de lo que salió el pedido.
 }
