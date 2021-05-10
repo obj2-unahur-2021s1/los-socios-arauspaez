174af12185b35b45910da0f2fe311585723cfa11
lucianoaraus <lucianoaraus97@gmail.com>
2021-05-09 22:14:40 -0300

    Update: Codigo hecho. Avanazar con los Tests

--- src/main/kotlin/ar/edu/unahur/obj2/socios/Cliente.kt
+++ src/main/kotlin/ar/edu/unahur/obj2/socios/Cliente.kt
@@ -1,4 +1,62 @@
 package ar.edu.unahur.obj2.socios
 
-class Cliente {
+class Cliente(var estadoDeAnimo: EstadoDeAnimo,var bolsillo: Double,var barrio: Barrio) {
+    lateinit var pedido : Pedido
+    fun hacerPedido(comida: Pedido){ this.pedido = comida }
+    fun consultarPropina() = this.estadoDeAnimo.propina(pedido,bolsillo) //TRATAR DE SACAR BOLSILLO DEL PARAMETRO YA QUE SOLO SE UTILIZA EN 1 ESTADO DE ANIMO
+    fun consultarPropinaBarrio() = this.barrio.cuenta(this.consultarPropina())
+    fun cambiarBarrio(nuevoBarrio: Barrio) { barrio = nuevoBarrio}
+    fun cambiarEstadoDeAnimo(nuevoEstado: EstadoDeAnimo) { estadoDeAnimo = nuevoEstado }
+    fun cambiarBolsillo(nuevoBolsillo: Double) { bolsillo = nuevoBolsillo }
 }
+interface EstadoDeAnimo {
+    fun propina(pedido: Pedido,bolsillo: Double) : Double
+}
+
+class Enojado() : EstadoDeAnimo {
+    override fun propina(pedido: Pedido,bolsillo: Double) = 0.0 //Pedido
+}
+
+class Feliz() : EstadoDeAnimo {
+    override fun propina(pedido: Pedido,bolsillo: Double) = pedido.precio * 0.25 //Deja como propina el 25% de lo que salió el pedido
+}
+
+class Indiferente() : EstadoDeAnimo {
+    override fun propina(pedido: Pedido,bolsillo: Double) = bolsillo //Deja como propina lo que tenga en el bolsillo.
+}
+
+class Resfriado() : EstadoDeAnimo {
+    override fun propina(pedido: Pedido,bolsillo: Double) = pedido.precio//Le invade el sentimiento de culpa y le deja de propina el 100% de lo que salió el pedido.
+}
+
+class Pedido (val nombre: String, val precio: Double){
+    //Implementar en un nuevo archivo.
+}
+
+abstract class Barrio{
+    //Implementar en un nuevo archivo.
+    abstract fun cuenta(monto: Double): Double
+}
+
+object lasRosas : Barrio() {
+    override fun cuenta(monto: Double) = monto + 50
+}
+
+object lasLauchas : Barrio() {
+    override fun cuenta(monto: Double) = monto / 2
+}
+
+object barrioVerde : Barrio() {
+    override fun cuenta(monto: Double): Double {
+        val montoBarrio = when {
+            monto <= 200 -> 200.0
+            else -> monto
+        }
+        return montoBarrio
+    }
+}
+
+object lasTorres : Barrio() {
+    override fun cuenta(monto: Double) = monto
+}
+
